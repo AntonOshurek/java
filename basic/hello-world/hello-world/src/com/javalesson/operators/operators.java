0cb48519e155fe98ea52f3141d0b1387bc491882
Anton Oshurek <oshurekdev@proton.me>
2024-06-01 13:55:45 +0200

    operators 2

--- basic/hello-world/hello-world/src/com/javalesson/operators/operators.java
+++ basic/hello-world/hello-world/src/com/javalesson/operators/operators.java
@@ -11,21 +11,39 @@ public class operators {
         double divide = a / b;
         double remainderOfTheDivision = a % b;
 
-        System.out.println("plus = " + plus);
-        System.out.println("minus = " + minus);
-        System.out.println("multiply = " + multiply);
-        System.out.println("divide = " + divide);
-        System.out.println("remainderOfTheDivision = " + remainderOfTheDivision);
+//        System.out.println("plus = " + plus);
+//        System.out.println("minus = " + minus);
+//        System.out.println("multiply = " + multiply);
+//        System.out.println("divide = " + divide);
+//        System.out.println("remainderOfTheDivision = " + remainderOfTheDivision);
 
 //        a = a + 10;
         a += 5; // short method
         a++;
         a--;
-        System.out.println("new value in a = "+a);
+//        System.out.println("new value in a = "+a);
 
         double n = 7;
         double m = 7;
-        System.out.println("res1 = " + 2*n++);
-        System.out.println("res2 = " + 2*++m);
+//        System.out.println("res1 = " + 2*n++);
+//        System.out.println("res2 = " + 2*++m);
+
+        int x = 3;
+        int y = 5;
+        int z = 8;
+
+//        == != < > <= >= && || ?:
+        boolean boolVal = x < y;
+        System.out.println("boolVal = " + boolVal);
+
+        boolean boolVal2 = y >= z;
+        boolean boolResult = boolVal2 || boolVal;
+        System.out.println("boolResult = " + boolResult);
+
+        int res = x < 5 ? x : y;
+        System.out.println("res = " + res);
+
+        System.out.println("NOT = " + !(x>y));
+
     }
 }
