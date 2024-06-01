b82c128895250d9f261b610209cb429a1307da23
Anton Oshurek <oshurekdev@proton.me>
2024-06-01 18:55:20 +0200

    if else statement

--- basic/hello-world/hello-world/src/com/javalesson/controlstatement/Controls.java
+++ basic/hello-world/hello-world/src/com/javalesson/controlstatement/Controls.java
@@ -2,6 +2,15 @@ package com.javalesson.controlstatement;
 
 public class Controls {
     public static void main(String[] args) {
-        
+        int testScore = 84;
+
+        if (testScore >= 90) {
+            System.out.println(">= 90");
+        } else if (testScore < 90 && testScore >= 80){
+            System.out.println("testScore < 90 && testScore >= 80");
+        }
+        else {
+            System.out.println("< 90");
+        }
     }
 }
