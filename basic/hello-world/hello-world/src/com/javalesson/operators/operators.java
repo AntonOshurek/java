f4100cbe97e8a4011a504989ed2c1c71a29b08fc
Anton Oshurek <oshurekdev@proton.me>
2024-06-01 13:30:15 +0200

    operators 1

--- basic/hello-world/hello-world/src/com/javalesson/operators/operators.java
+++ basic/hello-world/hello-world/src/com/javalesson/operators/operators.java
@@ -2,19 +2,30 @@ package com.javalesson.operators;
 
 public class operators {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
+        double a = 10;
+        double b = 3;
 
-        int plus = a + b;
-        int minus = a - b;
-        int multiply = a * b;
-        int divide = a / b;
-        int remainderOfTheDivision = a % b;
+        double plus = a + b;
+        double minus = a - b;
+        double multiply = a * b;
+        double divide = a / b;
+        double remainderOfTheDivision = a % b;
 
         System.out.println("plus = " + plus);
         System.out.println("minus = " + minus);
         System.out.println("multiply = " + multiply);
         System.out.println("divide = " + divide);
         System.out.println("remainderOfTheDivision = " + remainderOfTheDivision);
+
+//        a = a + 10;
+        a += 5; // short method
+        a++;
+        a--;
+        System.out.println("new value in a = "+a);
+
+        double n = 7;
+        double m = 7;
+        System.out.println("res1 = " + 2*n++);
+        System.out.println("res2 = " + 2*++m);
     }
 }
