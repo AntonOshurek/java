008cb85eda29e1e1767cbbe648de238aac2f7701
Anton Oshurek <oshurekdev@proton.me>
2024-06-09 21:05:48 +0200

    create getters and setters

--- basic/oop/src/com/javalesson/oop/Dog.java
+++ basic/oop/src/com/javalesson/oop/Dog.java
@@ -5,4 +5,36 @@ public class Dog {
     private int tail = 1;
     private String name;
     private String breed;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPaws() {
+        return paws;
+    }
+
+    public void setPaws(int paws) {
+        this.paws = paws;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public void setTail(int tail) {
+        this.tail = tail;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
